c13bb52062ac7b787875bbfb49ecf0c10b07d8bd
PSJ <PSJ@PSJ-PC>
2016-09-01 09:36:11 +0900

    JDBCUtil 클래스 추가

---
+++ Yena_GongJu/src/ldcc/board/dao/JDBCUtil.java
@@ -0,0 +1,66 @@
+package ldcc.board.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class JDBCUtil {
+
+	public static Connection getConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			return DriverManager.getConnection("jdbc:mysql://192.168.211.95:3306/mysql", "root", "1234");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static void close(PreparedStatement stmt, Connection con) {
+		try {
+			if (stmt != null)
+				stmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			stmt = null;
+		}
+		try {
+			if (con != null)
+				con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			con = null;
+		}
+	}
+
+	public static void close(ResultSet rst, PreparedStatement stmt, Connection con) {
+		try {
+			if (rst != null)
+				rst.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			rst = null;
+		}
+		try {
+			if (stmt != null)
+				stmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			stmt = null;
+		}
+		try {
+			if (con != null)
+				con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			con = null;
+		}
+	}
+
+}
