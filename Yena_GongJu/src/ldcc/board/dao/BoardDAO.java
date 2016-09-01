91379528edac76eac0c17468df24352e9cf5b9d9
PSJ <PSJ@PSJ-PC>
2016-09-01 17:20:18 +0900

    PostDAO, BoardDAO 메소드 구현

--- Yena_GongJu/src/ldcc/board/dao/BoardDAO.java
+++ Yena_GongJu/src/ldcc/board/dao/BoardDAO.java
@@ -1,5 +1,40 @@
 package ldcc.board.dao;
 
-public class BoardDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import ldcc.board.vo.Board;
+
+public class BoardDAO {
+	private final String getListSQL = "select * from BOARD order by BOARD_CODE desc";
+
+	public List<Board> doGetList() {
+		Connection con = null;
+		PreparedStatement stmt = null;
+		ResultSet rst = null;
+		List<Board> boardList = new ArrayList<Board>();
+
+		try {
+			con = JDBCUtil.getConnection();
+			stmt = con.prepareStatement(this.getListSQL);
+			rst = stmt.executeQuery();
+
+			while (rst.next()) {
+				Board board = new Board();
+				board.setBoard_code(rst.getInt(1));
+				board.setBoard_name(rst.getString(2));
+				boardList.add(board);
+			}
+		} catch (SQLException e) {
+			System.out.println("BoardDAO.doGetList() error : " + e.getMessage());
+		} finally {
+			JDBCUtil.close(rst, stmt, con);
+		}
+
+		return boardList;
+	}
 }
