d23189317668dd1e00708b391667c6f5e3637eb6
PSJ <PSJ@PSJ-PC>
2016-09-12 13:01:25 +0900

    URI front 추가 ("/"에서 "/post?action=list" 로 우회)

---
+++ Yena_GongJu/src/ldcc/board/controller/FrontServlet.java
@@ -0,0 +1,49 @@
+package ldcc.board.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FrontServlet
+ */
+@WebServlet("/FrontServlet")
+public class FrontServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public FrontServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=euc-kr");
+		PrintWriter out = response.getWriter();
+		out.println("<script>");
+		out.println("location.href=\"post?action=list\"");
+		out.println("</script>");
+		out.close();
+	}
+}
